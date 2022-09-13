db6089e6c4f88d0c168359e3d8c13ceaadc8293d
AsSpiriN16rus <bygaga357@yandex.ru>
2022-09-13 14:39:20 +0300

    add project

---
+++ src/main/java/org/javaproteam27/socialnetwork/repository/PersonRepository.java
@@ -0,0 +1,154 @@
+package org.javaproteam27.socialnetwork.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.javaproteam27.socialnetwork.handler.exception.EntityNotFoundException;
+import org.javaproteam27.socialnetwork.mapper.PersonMapper;
+import org.javaproteam27.socialnetwork.model.entity.Person;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class PersonRepository {
+
+    private final RowMapper<Person> rowMapper = new PersonMapper();
+    private final JdbcTemplate jdbcTemplate;
+
+    public void save(Person person) {
+
+        String sql = "insert into person(first_name, last_name, reg_date, birth_date, email, phone, " +
+                "password, photo, about, city, country, confirmation_code, is_approved, messages_permission, " +
+                "last_online_time, is_blocked) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        jdbcTemplate.update(sql, person.getFirstName(), person.getLastName(), person.getRegDate(),
+                person.getBirthDate(), person.getEmail(), person.getPhone(), person.getPassword(),
+                person.getPhoto(), person.getAbout(), person.getCity(), person.getCountry(), person.getConfirmationCode(),
+                person.getIsApproved(), person.getMessagesPermission(), person.getLastOnlineTime(),
+                person.getIsBlocked());
+    }
+
+    public Person findById(int id) {
+        try {
+            String sql = "select * from person where id = ?";
+            return jdbcTemplate.queryForObject(sql, rowMapper, id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new EntityNotFoundException("person id = " + id);
+        }
+    }
+
+    public List<Person> getFriendsPersonById(Integer id){
+        try {
+            String sql = "SELECT * FROM friendship f\n" +
+                    "join friendship_status fs on fs.id=f.status_id\n" +
+                    "join person p on f.dst_person_id=p.id\n" +
+                    "where fs.code = 'FRIEND' and src_person_id = ?";
+            return jdbcTemplate.query(sql, rowMapper, id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new EntityNotFoundException("person id = " + id);
+        }
+    }
+
+
+    public Person findByEmail(String email) {
+        try {
+            String sql = "select * from person where email like ?";
+            return jdbcTemplate.queryForObject(sql, rowMapper, email);
+        } catch (EmptyResultDataAccessException e) {
+            throw new EntityNotFoundException("person email = " + email);
+        }
+    }
+
+    public Integer count() {
+        try {
+            String sql = "select count(*) from person";
+            return jdbcTemplate.queryForObject(sql, Integer.class);
+        } catch (EmptyResultDataAccessException e) {
+            return 0; // todo обработать исключение
+        }
+    }
+    public Person getPersonById(Integer id){
+        return jdbcTemplate.queryForObject("SELECT * FROM person WHERE id = " + id, Person.class);
+    }
+
+    public List<Person> findPeople (Person authorizedPerson, String firstName, String lastName, Integer ageFrom,
+                                    Integer ageTo, String city, String country) {
+
+        //TODO: JOOQ + Filter blocked people
+
+        ArrayList<String> queryParts = new ArrayList<>();
+
+        if(firstName != null) {
+            queryParts.add("first_name = '" + firstName + "'");
+        }
+
+        if(lastName != null) {
+            queryParts.add("last_name = '" + lastName + "'");
+        }
+
+        if(ageFrom != null) {
+            queryParts.add("date_part('year', age(birth_date))::int > " + ageFrom);
+        }
+
+        if(ageTo != null) {
+            queryParts.add("date_part('year', age(birth_date))::int < " + ageTo);
+        }
+
+        if(city != null) {
+            queryParts.add("city = '" + city + "'");
+        }
+
+        if(country != null) {
+            queryParts.add("country = '" + country + "'");
+        }
+
+        String buildQuery = "SELECT * FROM person WHERE " + String.join(" AND ", queryParts) + ";";
+        return jdbcTemplate.query(buildQuery, rowMapper);
+    }
+
+    public List<Person> getFriendsPersonById(String name,Integer id){
+        try {
+            String sql;
+            if (name != null && name.length() != 0){
+                sql = "SELECT * FROM person p \n" +
+                        "join friendship f on f.dst_person_id = p.id\n" +
+                        "join friendship_status fs on fs.id = f.status_id\n" +
+                        "where fs.code = 'FRIEND' and src_person_id = ?" +
+                        "and first_name like '%" + name  +"%'";
+            }else {
+                sql = "SELECT * FROM person p \n" +
+                        "join friendship f on f.dst_person_id = p.id\n" +
+                        "join friendship_status fs on fs.id = f.status_id\n" +
+                        "where fs.code = 'FRIEND' and src_person_id = ?";
+            }
+
+            return jdbcTemplate.query(sql, rowMapper, id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new EntityNotFoundException("person id = " + id);
+        }
+    }
+
+    public List<Person> getApplicationsFriendsPersonById(String name,Integer id){
+        try {
+            String sql;
+            if (name != null && name.length() != 0){
+                sql = "SELECT * FROM person p \n" +
+                        "join friendship f on f.dst_person_id = p.id\n" +
+                        "join friendship_status fs on fs.id = f.status_id\n" +
+                        "where fs.code = 'REQUEST' and src_person_id = ?" +
+                        "and first_name like '%" + name  +"%'";
+            }else {
+                sql = "SELECT * FROM person p \n" +
+                        "join friendship f on f.dst_person_id = p.id\n" +
+                        "join friendship_status fs on fs.id = f.status_id\n" +
+                        "where fs.code = 'REQUEST' and src_person_id = ?";
+            }
+            return jdbcTemplate.query(sql, rowMapper, id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new EntityNotFoundException("person id = " + id);
+        }
+    }
+}
---
+++ src/main/java/org/javaproteam27/socialnetwork/repository/PostRepository.java
@@ -0,0 +1,118 @@
+package org.javaproteam27.socialnetwork.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.javaproteam27.socialnetwork.handler.exception.ErrorException;
+import org.javaproteam27.socialnetwork.model.entity.Post;
+import org.javaproteam27.socialnetwork.mapper.PostMapper;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class PostRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public Integer addPost(long time, int authorId, String title, String postText) {
+        Integer postId;
+        try {
+            String sqlQuery = "INSERT INTO post (time, author_id, title, post_text) " +
+                    "VALUES ('" + new Timestamp(time) + "', " + authorId + ", '" + title + "', '" + postText + "')";
+            KeyHolder keyHolder = new GeneratedKeyHolder();
+            jdbcTemplate.update(connection -> connection.prepareStatement(sqlQuery, new String[]{"id"}), keyHolder);
+            postId = (Integer) keyHolder.getKey();
+        } catch (DataAccessException exception){
+            throw new ErrorException(exception.getMessage());
+        }
+        return postId;
+    }
+
+    public List<Post> findAllUserPosts(int authorId, int offset, int limit) {
+        List<Post> retList;
+        try {
+            retList = jdbcTemplate.query("SELECT * FROM post WHERE author_id = " + authorId
+                    + " AND is_deleted is false" + " LIMIT " + limit + " OFFSET " + offset, new PostMapper());
+        } catch (DataAccessException exception){
+            throw new ErrorException(exception.getMessage());
+        }
+        return retList;
+    }
+
+    public Boolean deletePostById(int postId) {
+        Boolean retValue;
+        try {
+            retValue = (jdbcTemplate.update("UPDATE post SET is_deleted = true WHERE id = ?", postId) == 1);
+        } catch (DataAccessException exception){
+            throw new ErrorException(exception.getMessage());
+        }
+        return retValue;
+    }
+
+    public Boolean updatePostById(int postId, String title, String postText) {
+        Boolean retValue;
+        try {
+            retValue = (jdbcTemplate.update("UPDATE post SET title = ?, post_text = ? WHERE id = ?", title,
+                    postText, postId) == 1);
+        } catch (DataAccessException exception){
+            throw new ErrorException(exception.getMessage());
+        }
+        return retValue;
+    }
+
+    public Post findPostById(int postId) {
+        Post post;
+        try {
+            post = jdbcTemplate.queryForObject("SELECT * FROM post WHERE id = ?"
+                    , new Object[]{postId}, new PostMapper());
+        }
+        catch (DataAccessException exception){
+            throw new ErrorException(exception.getMessage());
+        }
+        return post;
+    }
+    public List<Post> findAllPublishedPosts(int offset, int limit){
+        List<Post> retList;
+        try {
+            retList = jdbcTemplate.query("SELECT * FROM post WHERE time <= CURRENT_TIMESTAMP " +
+                    "AND is_deleted is false LIMIT " + limit + " OFFSET " + offset,new PostMapper());
+            //                "SELECT * FROM post WHERE post_text LIKE '%" + postText + "%'"
+        } catch (DataAccessException exception){
+            throw new ErrorException(exception.getMessage());
+        }
+
+        return retList;
+    }
+
+    public List<Post> findPost(String text, Long dateFrom, Long dateTo) {
+        ArrayList<String> queryParts = new ArrayList<>();
+
+        if (text != null) {
+            queryParts.add("post_text LIKE '%" + text + "%'");
+        }
+
+        if (dateFrom != null) {
+            LocalDate dateFromParsed =
+                    Instant.ofEpochMilli(dateFrom).atZone(ZoneId.systemDefault()).toLocalDate();
+            queryParts.add("time > '" + dateFromParsed + "'::date");
+        }
+
+        if (dateTo != null) {
+            LocalDate dateToParsed =
+                    Instant.ofEpochMilli(dateTo).atZone(ZoneId.systemDefault()).toLocalDate();
+            queryParts.add("time < '" + dateToParsed + "'::date");
+        }
+
+        String buildQuery = "SELECT * FROM post WHERE " + String.join(" AND ", queryParts) + ";";
+        return jdbcTemplate.query(buildQuery, new PostMapper());
+    }
+
+}
